91daeca989ee49b899fe62a310e75166d9e06185
ke52usel@studserv.uni-leipzig.de <56174409+fansel@users.noreply.github.com>
2025-01-05 00:51:37 +0100

    update bot

--- bot.py
+++ bot.py
@@ -107,7 +107,11 @@ async def start_bot():
     )
 
 if __name__ == "__main__":
-    loop = asyncio.get_event_loop()
-    loop.create_task(start_bot())
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=5000)
+    try:
+        loop = asyncio.get_running_loop()
+    except RuntimeError:
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
+
+    uvicorn.run("bot:app", host="0.0.0.0", port=5000)
