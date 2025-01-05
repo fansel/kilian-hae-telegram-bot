5cb60b71776cf7e8ee19ea1bd620f1a99c1c8915
ke52usel@studserv.uni-leipzig.de <56174409+fansel@users.noreply.github.com>
2025-01-05 01:37:40 +0100

    update bot

--- bot.py
+++ bot.py
@@ -137,10 +137,7 @@ async def start_bot():
     )
 
 if __name__ == "__main__":
-    try:
-        loop = asyncio.get_running_loop()
-    except RuntimeError:
-        loop = asyncio.new_event_loop()
-        asyncio.set_event_loop(loop)
+    # Bestehenden Event-Loop verwenden oder neuen erstellen
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(start_bot())
 
-    asyncio.run(start_bot())
